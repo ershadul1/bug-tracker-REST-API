65a87eb03092b59f7f1ab10e107a543a3a1cda9f
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-15 15:37:44 +0600

    Add routes for user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      resource :users, only: [:create, :update]
+      post '/login', to: "users#login"
+      get '/auto_login', to: "users#auto_login"
+    end
+  end
 end
