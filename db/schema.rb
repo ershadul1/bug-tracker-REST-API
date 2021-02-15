9d9f497db4f780df7f219ccbca283e701811bf7a
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-15 22:53:55 +0600

    Generate Bug model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_152814) do
+ActiveRecord::Schema.define(version: 2021_02_15_164315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bugs", force: :cascade do |t|
+    t.integer "project_id"
+    t.integer "author_id"
+    t.string "title"
+    t.text "description"
+    t.string "priority", default: "medium"
+    t.string "status", default: "open"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
