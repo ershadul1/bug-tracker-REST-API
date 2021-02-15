48083fa8c2c7a4ca2b663f8bdb70fdc23c8688dd
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-15 23:49:44 +0600

    Generate Assign model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_171918) do
+ActiveRecord::Schema.define(version: 2021_02_15_174845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assigns", force: :cascade do |t|
+    t.integer "bug_id"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bug_id"], name: "index_assigns_on_bug_id"
+    t.index ["user_id"], name: "index_assigns_on_user_id"
+  end
 
   create_table "bugs", force: :cascade do |t|
     t.integer "project_id"
