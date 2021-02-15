458c18f86ed7121ceef4da26a68b6ff06238e4a8
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-15 23:15:15 +0600

    Add show method in bugs controller

--- app/controllers/api/v1/bugs_controller.rb
+++ app/controllers/api/v1/bugs_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class BugsController < ApplicationController
       
-      def index
+      def show
         bugs = Bug.order('created_at DESC');
         render json: {status: 'SUCCESS', message:'Loaded bug reports', data:bugs},status: :ok
       end
