f2925cd53e6c50ff0eaf2ab85ff1f9a4c7d58ffa
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-16 18:35:08 +0600

    Create association between Bug and Resolve model

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,7 +2,7 @@ class Bug < ApplicationRecord
   belongs_to :project
   has_many :comments, foreign_key: :bug_id, dependent: :destroy
   has_one :assign, foreign_key: :bug_id, dependent: :destroy
-
+  has_one :resolve, foreign_key: :bug_id, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
 end
--- app/models/resolve.rb
+++ app/models/resolve.rb
@@ -1,2 +1,5 @@
 class Resolve < ApplicationRecord
+  validates_uniqueness_of :bug_id
+  belongs_to :user
+  belongs_to :bug
 end
