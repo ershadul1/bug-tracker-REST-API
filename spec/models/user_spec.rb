985a4ad34a72588a4cabfc69234bdaf663bdecb5
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-16 19:00:40 +0600

    Add tests for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'Validates Username' do
+    it 'Should be valid' do
+      user = User.new(username: 'rayhan', password: '1234')
+      expect(user.save).to be true
+    end
+
+    it 'should not be valid' do
+      user = User.new(username: '', password: '1234')
+      expect(user.save).not_to be true
+    end
+  end
+
+  context 'Validates Password' do
+    it 'Should be valid' do
+      user = User.new(username: 'rayhan', password: '1234')
+      expect(user.save).to be true
+    end
+
+    it 'should not be valid' do
+      user = User.new(username: 'rayhan', password: '')
+      expect(user.save).not_to be true
+    end
+  end
+
+end
