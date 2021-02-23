ac3796e52c5c79b8ab5f3fc925333336cc66016c
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-23 18:53:37 +0600

    Add status to the json response

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,9 +7,9 @@ module Api
         @user = User.create(user_params)
         if @user.valid?
           token = encode_token({ user_id: @user.id })
-          render json: { user: @user, token: token }
+          render json: { status: 'SUCCESS', user: @user, token: token }
         else
-          render json: { error: 'Invalid username or password' }
+          render json: { status: 'FAILURE', error: 'Invalid username or password' }
         end
       end
 
@@ -18,9 +18,9 @@ module Api
 
         if @user&.authenticate(params[:password])
           token = encode_token({ user_id: @user.id })
-          render json: { user: @user, token: token }
+          render json: { status: 'SUCCESS', user: @user, token: token }
         else
-          render json: { error: 'Invalid username or password' }
+          render json: { status: 'FAILURE', error: 'Invalid username or password' }
         end
       end
 
