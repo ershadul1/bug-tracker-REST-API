3929c1406f17ba9c90abc224186299a285200783
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-23 17:27:55 +0600

    Add users info of commenters

--- app/controllers/api/v1/bugs_controller.rb
+++ app/controllers/api/v1/bugs_controller.rb
@@ -10,6 +10,7 @@ module Api
         bug = Bug.find_by(id: params[:id])
         render json: { status: 'SUCCESS', message: 'Loaded bug report', 
         data: {bug: bug, comments: bug.comments, assign: bug.assign,
+        users: User.where(id: bug.comments.pluck(:user_id)),
         resolve: bug.resolve, assignee_name: bug.assign ? User.find_by(id: bug.assign.user_id).username : nil,
         author_name: User.find_by(id: bug.author_id).username }}, status: :ok
       end
