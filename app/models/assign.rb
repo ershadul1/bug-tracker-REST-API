dc2972361cf1802ef40c494dc3219da0cbd81792
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-25 13:38:31 +0600

    Add callbacks to change bug status in assign model

--- app/models/assign.rb
+++ app/models/assign.rb
@@ -1,5 +1,18 @@
 class Assign < ApplicationRecord
   validates_uniqueness_of :bug_id
+
   belongs_to :user
   belongs_to :bug
+
+  after_save do
+    bug = Bug.find_by(id: bug_id)
+    bug.status = 'assigned'
+    bug.save
+  end
+
+  after_destroy do
+    bug = Bug.find_by(id: bug_id)
+    bug.status = 'open'
+    bug.save
+  end
 end
