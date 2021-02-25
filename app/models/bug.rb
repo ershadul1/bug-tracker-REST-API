349f2bdec30774b9c7c67da439fc661487f860d6
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-25 13:37:14 +0600

    Add details method in bug model

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,8 +1,15 @@
 class Bug < ApplicationRecord
   belongs_to :project
+  belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: :bug_id, dependent: :destroy
   has_one :assign, foreign_key: :bug_id, dependent: :destroy
   has_one :resolve, foreign_key: :bug_id, dependent: :destroy
   validates :title, presence: true
   validates :description, presence: true
+
+  def details
+    { bug: self, author_name: author.username, comments: comments,
+      assign: assign, assignee_name: assign ? assign.user.username : nil,
+      resolve: resolve }
+  end
 end
