ce5954cafb1487dad824aa461ad4ca441fcb9518
ershadul1 <ershadul.rayhan@gmail.com>
2021-02-25 13:41:28 +0600

    Remove login, autologin methods from users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,49 +1,40 @@
 module Api
   module V1
     class UsersController < ApplicationController
-      before_action :authorized, only: %i[auto_login update]
+      before_action :authorized, only: %i[update show]
+
+      def show
+        render json: { status: 'SUCCESS', user: @user }, status: :ok
+      end
 
       def create
         @user = User.create(user_params)
+
         if @user.valid?
           token = encode_token({ user_id: @user.id })
-          render json: { status: 'SUCCESS', user: @user, token: token }
+          render json: { status: 'SUCCESS', user: @user, token: token }, status: :ok
         else
-          render json: { status: 'FAILURE', error: 'Invalid username or password' }
+          render json: { status: 'FAILURE', error: 'Invalid username or password' },
+                 status: :unprocessable_entity
         end
-      end
-
-      def login
-        @user = User.find_by(username: params[:username])
-
-        if @user&.authenticate(params[:password])
-          token = encode_token({ user_id: @user.id })
-          render json: { status: 'SUCCESS', user: @user, token: token }
-        else
-          render json: { status: 'FAILURE', error: 'Invalid username or password' }
-        end
-      end
-
-      def auto_login
-        render json: @user
       end
 
       def update
         @user = User.find_by(id: params[:id])
-        @user.update(user_params)
 
-        if @user.save
+        if @user.update(user_params)
           token = encode_token({ user_id: @user.id })
-          render json: { user: @user, token: token }
+          render json: { status: 'SUCCESS', user: @user, token: token }, status: :ok
         else
-          render json: { error: 'Unable to change username and password' }
+          render json: { status: 'FAILURE', error: 'Unable to change username and password' },
+                 status: :unprocessable_entity
         end
       end
 
       private
 
       def user_params
-        params.permit(:username, :password, :id)
+        params.permit(:username, :password)
       end
     end
   end
